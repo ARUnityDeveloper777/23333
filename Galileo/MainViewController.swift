c0ceccf72dc39ecf0345373105d5e7271365f90a
chic <chic@chicdeiMac.local>
2017-06-19 10:19:35 +0100

    Init

--- Galileo/MainViewController.swift
+++ Galileo/MainViewController.swift
@@ -1,14 +1,15 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Galileo
 //
 //  Created by chic on 15/06/2017.
 //  Copyright © 2017 chic. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     
     @IBOutlet weak var splashView: UIView!
     
@@ -16,16 +17,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        splashView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(splashTapped)))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func splashTapped(sender:UITapGestureRecognizer){
-        [self .performSegue(withIdentifier: "segueMainController", sender: self)]
-    }
+    
+    
 }
-
